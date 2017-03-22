72823179cc763c4b1f4d6be9420b611a75bbf944
Ryu Yamamoto <ron_robotics@yahoo.co.jp>
2017-03-22 21:12:48 +0900

    インデントの修正

--- plugin/MotionGeneratorPlugin.cpp
+++ plugin/MotionGeneratorPlugin.cpp
@@ -20,60 +20,60 @@ const int sign[] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
 
 class MotionGeneratorPlugin : public Plugin
 {
-private:
-	Kinematics *kine;
-	cit::Link ulink[Const::LINK_NUM];
-	cit::Link RFLink, LFLink;
-	float servo_angle[Const::LINK_NUM];
-public:
-    
-    MotionGeneratorPlugin() : Plugin("MotionGenerator")
-    {
-        require("Body");
+	private:
+		Kinematics *kine;
+		cit::Link ulink[Const::LINK_NUM];
+		cit::Link RFLink, LFLink;
+		float servo_angle[Const::LINK_NUM];
+	public:
+
+		MotionGeneratorPlugin() : Plugin("MotionGenerator")
+	{
+		require("Body");
 
 		cit::initLink(ulink);
 		kine = new Kinematics(ulink);
 
 		for(int i=0;i<Const::LINK_NUM;i++)
 			servo_angle[i] = 0.0f;
-    }
-    
-    virtual bool initialize()
-    {
-        ToolBar* bar = new ToolBar("MotionGenerator");
-        bar->addButton("Increment")
-				->sigClicked().connect(bind(&MotionGeneratorPlugin::onButtonClicked, this, +0.04));
-        bar->addButton("Decrement")
-                ->sigClicked().connect(bind(&MotionGeneratorPlugin::onButtonClicked, this, -0.04));
-        addToolBar(bar);
-
-        return true;
-    }
-#if 0
-	void getCurrentJointState()
-	{
-		ItemList<BodyItem> bodyItems = ItemTreeView::mainInstance()->selectedItems<BodyItem>();
-
-		BodyPtr body = bodyItems[i]->body();
-		for(int j=0;i<body->numJoints();i++)
-			servo_angle[i]
 	}
+
+		virtual bool initialize()
+		{
+			ToolBar* bar = new ToolBar("MotionGenerator");
+			bar->addButton("Increment")
+				->sigClicked().connect(bind(&MotionGeneratorPlugin::onButtonClicked, this, +0.04));
+			bar->addButton("Decrement")
+				->sigClicked().connect(bind(&MotionGeneratorPlugin::onButtonClicked, this, -0.04));
+			addToolBar(bar);
+
+			return true;
+		}
+#if 0
+		void getCurrentJointState()
+		{
+			ItemList<BodyItem> bodyItems = ItemTreeView::mainInstance()->selectedItems<BodyItem>();
+
+			BodyPtr body = bodyItems[i]->body();
+			for(int j=0;i<body->numJoints();i++)
+				servo_angle[i]
+		}
 #endif
 
-    void onButtonClicked(double dq)
-    {
-        ItemList<BodyItem> bodyItems = ItemTreeView::mainInstance()->selectedItems<BodyItem>();
-        //cout << body->numJoints() << endl;
-        for(size_t i=0; i < bodyItems.size(); ++i){
-			BodyPtr body = bodyItems[i]->body();
-			cout << body->numJoints() << endl;
-			for(int j=0; j < body->numJoints(); ++j){
-                cout << body->joint(j)->name() << endl;
-				body->joint(j)->q() += dq;
+		void onButtonClicked(double dq)
+		{
+			ItemList<BodyItem> bodyItems = ItemTreeView::mainInstance()->selectedItems<BodyItem>();
+			//cout << body->numJoints() << endl;
+			for(size_t i=0; i < bodyItems.size(); ++i){
+				BodyPtr body = bodyItems[i]->body();
+				cout << body->numJoints() << endl;
+				for(int j=0; j < body->numJoints(); ++j){
+					cout << body->joint(j)->name() << endl;
+					body->joint(j)->q() += dq;
+				}
+				bodyItems[i]->notifyKinematicStateChange(true);
 			}
-            bodyItems[i]->notifyKinematicStateChange(true);
-        }
-    }
+		}
 };
 
 CNOID_IMPLEMENT_PLUGIN_ENTRY(MotionGeneratorPlugin)
