849dd22bc057ac30d81d5684497c5561f5370c0a
RyuYamamoto <ron_robotics@yahoo.co.jp>
2017-03-23 19:33:11 +0900

    リファクタリング

--- plugin/MotionGeneratorPlugin.cpp
+++ plugin/MotionGeneratorPlugin.cpp
@@ -16,16 +16,6 @@
 using namespace std;
 using namespace cnoid;
 
-double deg2rad(double degree)
-{
-	return degree * M_PI / 180.0;
-}
-
-double rad2deg(double radian)
-{
-	return radian * 180.0 / M_PI;
-}
-
 //実機との符号調整
 const int sign[] = {-1,1,-1,-1,1,1,-1,-1,1,1,1,1, 1,1,1,1,1,1,1,1,1,1,1,1,1}; 
 
@@ -67,27 +57,10 @@ public:
 	{
 		ItemList<BodyItem> bodyItems = ItemTreeView::mainInstance()->selectedItems<BodyItem>();
 		BodyPtr body = bodyItems[0]->body();
-		cout << body->joint(0)->q() << endl;
-		// 左脚関節
-		servo_angle[0]	= body->joint(0)->q() * sign[0];
-		servo_angle[1]	= body->joint(1)->q() * sign[1];
-		servo_angle[2]  = body->joint(2)->q() * sign[2];
-		servo_angle[3]	= body->joint(3)->q() * sign[3];
-		servo_angle[4]	= body->joint(4)->q() * sign[4];
-		servo_angle[5]	= body->joint(5)->q() * sign[5];
-		// 右脚関節
-		servo_angle[6]	= body->joint(6)->q() * sign[6];
-		servo_angle[7]	= body->joint(7)->q() * sign[7];
-		servo_angle[8]	= body->joint(8)->q() * sign[8];
-		servo_angle[9]	= body->joint(9)->q() * sign[9];
-		servo_angle[10] = body->joint(10)->q() * sign[10];
-		servo_angle[11] = body->joint(11)->q() * sign[11];
-		// 左腕
-		servo_angle[14] = body->joint(14)->q() * sign[14];
-		servo_angle[15] = body->joint(15)->q() * sign[15];
-		// 右腕
-		servo_angle[16] = body->joint(16)->q() * sign[16];
-		servo_angle[17] = body->joint(17)->q() * sign[17];
+		for(int i=0;i<12;i++)
+			servo_angle[i] = body->joint(i)->q() * sign[i];
+		for(int i=14;i<18;i++)
+			servo_angle[i] = body->joint(i)->q() * sign[i];
 
 		//順運動学計算
 		kine->setJointAngle(servo_angle);
@@ -105,20 +78,17 @@ public:
 
 		RFLink.p(0) += dq; 
         if(kine->calcInverseKinematics(servo_angle, RFLink, LFLink)){
-			// 左脚
-			body->joint(0)->q() = servo_angle[0] * sign[0];
-			body->joint(1)->q() = servo_angle[1] * sign[1]; body->joint(18)->q() = body->joint(1)->q(); body->joint(19)->q() = body->joint(1)->q() * (-1);
-			body->joint(2)->q() = servo_angle[2] * sign[2]; body->joint(12)->q() = body->joint(2)->q() * (-1); body->joint(13)->q() = body->joint(2)->q();
-			body->joint(3)->q() = servo_angle[3] * sign[3];
-			body->joint(4)->q() = servo_angle[4] * sign[4];
-			body->joint(5)->q() = servo_angle[5] * sign[5];
-			// 右脚
-			body->joint(6)->q() = servo_angle[6] * sign[6];
-			body->joint(7)->q() = servo_angle[7] * sign[7]; body->joint(23)->q() = body->joint(7)->q(); body->joint(24)->q() = body->joint(7)->q();
-			body->joint(8)->q() = servo_angle[8] * sign[8]; body->joint(20)->q() = body->joint(8)->q(); body->joint(21)->q() = body->joint(8)->q() * (-1);
-			body->joint(9)->q() = servo_angle[9] * sign[9]; 
-			body->joint(10)->q() = servo_angle[10] * sign[10];
-			body->joint(11)->q() = servo_angle[11] * sign[11];
+			for(int i=0;i<12;i++)
+				body->joint(i)->q() = servo_angle[i] * sign[i];
+			
+			body->joint(12)->q() = body->joint(2)->q() * (-1);
+			body->joint(13)->q() = body->joint(2)->q();
+			body->joint(18)->q() = body->joint(1)->q();
+			body->joint(19)->q() = body->joint(1)->q() * (-1);
+			body->joint(20)->q() = body->joint(8)->q();
+			body->joint(21)->q() = body->joint(8)->q() * (-1);
+			body->joint(23)->q() = body->joint(7)->q();
+			body->joint(24)->q() = body->joint(7)->q();
 		}
 		bodyItems[0]->notifyKinematicStateChange(true);
     }
